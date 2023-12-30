bf81ee3c421b6136dafe3f2cc313ae7c2e62dd6f
hugosique <otahugo02@gmail.com>
2023-12-30 13:47:49 -0300

    grades mock style

--- src/components/Grade.tsx
+++ src/components/Grade.tsx
@@ -1,6 +1,15 @@
 import '../styles/Grade.scss';
 
+import ChartIcon from '../assets/icons/Chart.svg';
+
 export default (props: any) =>
     <div className='grade'>
-        Olá Mundo
+        <div className='grade__info'>
+            <h1>Artes</h1>
+            <h2>28/04/2022</h2>
+        </div>
+        <div className='grade__note'>
+            <img src={ChartIcon} alt="Icon" />
+            <span>Nota: 5</span>
+        </div>
     </div>
