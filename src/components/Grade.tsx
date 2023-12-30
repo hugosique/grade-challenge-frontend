30ae9ea5c8bb91bc9ac1c0c0f2a86fb71c9eccc2
hugosique <otahugo02@gmail.com>
2023-12-30 13:53:24 -0300

    grade trash inclusion

--- src/components/Grade.tsx
+++ src/components/Grade.tsx
@@ -1,15 +1,20 @@
 import '../styles/Grade.scss';
 
 import ChartIcon from '../assets/icons/Chart.svg';
+import TrashIcon from '../assets/icons/Trash.svg';
 
 export default (props: any) =>
     <div className='grade'>
-        <div className='grade__info'>
-            <h1>Artes</h1>
-            <h2>28/04/2022</h2>
+        <div className='grade__content'>
+            <div className='grade__content--info'>
+                <h1>Artes</h1>
+                <h2>28/04/2022</h2>
+            </div>
+            <div className='grade__content--note'>
+                <img src={ChartIcon} alt="ChartIcon" />
+                <span>Nota: 5</span>
+            </div>
         </div>
-        <div className='grade__note'>
-            <img src={ChartIcon} alt="Icon" />
-            <span>Nota: 5</span>
-        </div>
+
+        <img src={TrashIcon} alt="TrashIcon" />
     </div>
